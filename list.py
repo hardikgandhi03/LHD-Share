fb795a71e05fcc79e4307d340fea37c6d2e102e3
Hardik Gandhi <62399245+hardikgandhi03@users.noreply.github.com>
2021-03-31 18:13:40 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,5 @@
+guilds = ["BLAHAJGang", "EddieHub", "Developer Student Community", "Wind in our chest hair","CUSAT","CodXCrypt","inaxia","Programmers Universal Group","EquiCode"]
+  
+guilds.sort()
+  
+print(guilds)
