cd36fd2efa2a06b02b33f9c4014ef04e0c3d8856
Hardik Gandhi <62399245+hardikgandhi03@users.noreply.github.com>
2021-03-31 21:10:42 +0530

    Add files via upload

---
+++ CLI.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    char r;
+    cout<<"Choose any number and enter it: ";
+    cin>>n;
+    cout<<"Now double the number in ur mind.\nThen Add 10 to it.\nNow after adding 10 divide it by 2.\nNow subtract the result by original number.";
+    cout<<"Now enter Y";
+    cin>>r;
+    cout<<"\nNow see the magic...The current value in your mind is 5!!! Right!!! Hahahaha XD XD";
+}
