e1fae8fa948e0988da959d1c6c30a639e5530db4
Hardik Gandhi <62399245+hardikgandhi03@users.noreply.github.com>
2021-04-04 17:57:57 +0530

    Delete list.py

--- list.py
+++
@@ -1,5 +0,0 @@
-guilds = ["BLAHAJGang", "EddieHub", "Developer Student Community", "Wind in our chest hair","CUSAT","CodXCrypt","inaxia","Programmers Universal Group","EquiCode"]
-  
-guilds.sort()
-  
-print(guilds)
